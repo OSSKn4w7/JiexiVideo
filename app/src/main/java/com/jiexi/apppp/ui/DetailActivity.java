d8d5f323fc1d031d9bbdd6f3b5fa5fdb777b294e
OSSKn4w7 <osskn4w7@outlook.com>
2026-06-24 11:44:32 +0800

    补充ProgressBar import

--- app/src/main/java/com/jiexi/apppp/ui/DetailActivity.java
+++ app/src/main/java/com/jiexi/apppp/ui/DetailActivity.java
@@ -26,6 +26,7 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
