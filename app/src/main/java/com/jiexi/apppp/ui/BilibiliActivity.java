f61048c53732be99ecc02f35c6a5154b99f24ef0
OSSKn4w7 <osskn4w7@outlook.com>
2026-06-24 12:11:04 +0800

    修复drawable旧颜色引用+补TextUtils import

--- app/src/main/java/com/jiexi/apppp/ui/BilibiliActivity.java
+++ app/src/main/java/com/jiexi/apppp/ui/BilibiliActivity.java
@@ -10,6 +10,7 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
